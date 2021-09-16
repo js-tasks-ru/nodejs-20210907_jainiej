ea9aa5f970662d845dba273179d34d33970558e6
Evgeniya <evgeniya@Evgeniyas-MacBook-Pro.local>
2021-09-16 23:10:59 +0300

    add solution to LimitSizeStream task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,22 @@
-const stream = require('stream');
-const LimitExceededError = require('./LimitExceededError');
+const stream = require("stream");
+const LimitExceededError = require("./LimitExceededError");
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
+  constructor({ limit, ...options }) {
     super(options);
+    this.limit = limit;
+    this.accumulatedBytes = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    const length = chunk.byteLength;
+    this.accumulatedBytes += length;
 
+    if (this.accumulatedBytes > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
