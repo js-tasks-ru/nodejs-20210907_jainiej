92f265827293632a929903210281f260accc80e0
Evgeniya <evgeniya@Evgeniyas-MacBook-Pro.local>
2021-09-23 01:09:03 +0300

    correct stream piping

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,10 +1,11 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable indent */
 /* eslint-disable object-curly-spacing */
 /* eslint-disable quotes */
-const url = require("url");
 const fs = require("fs");
 const http = require("http");
 const path = require("path");
+const LimitSizeStream = require("./LimitSizeStream");
 
 const server = new http.Server();
 
@@ -14,23 +15,55 @@ server.on("request", (req, res) => {
 
   const filepath = path.join(__dirname, "files", pathname);
 
+  if (pathname.includes("/")) {
+    res.statusCode = 400;
+    res.end("nested files are not allowed");
+    return;
+  }
+
   switch (req.method) {
     case "POST":
-      const writeStream = req.pipe(
-        fs.createWriteStream(filepath, { flags: "wx" })
-      );
+      const writeStream = fs.createWriteStream(filepath, { flags: "wx" });
+      const limitStream = new LimitSizeStream({ limit: 10000 });
+
+      req.pipe(limitStream).pipe(writeStream);
+
+      limitStream.on("error", (e) => {
+        if (e.code === "LIMIT_EXCEEDED") {
+          res.statusCode = 413;
+          res.end("max file size is exceeded");
+
+          fs.unlink(filepath, (e) => {
+            console.log(e);
+          });
+
+          writeStream.destroy();
+        }
+      });
 
       writeStream.on("error", (e) => {
         if (e.code === "EEXIST") {
           res.statusCode = 409;
           res.end("file already exists");
+        } else {
+          res.statusCode = 500;
+          res.end("server error");
         }
-        console.log(e);
+        limitStream.destroy();
       });
 
       writeStream.on("finish", () => {
-        res.statusCode = 200;
+        res.statusCode = 201;
         res.end("success");
+        return;
+      });
+
+      req.on("aborted", () => {
+        limitStream.destroy();
+        writeStream.destroy();
+        fs.unlink(filepath, (e) => {
+          console.log(e);
+        });
       });
       break;
 
